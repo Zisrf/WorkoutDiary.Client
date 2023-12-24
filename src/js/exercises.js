880925ee68ca83c8bba9d98b109ccc37b3f416c0
ZISRF <zeleninivans@gmail.com>
2023-12-24 17:42:53 +0300

    feat: add exercises fetching

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,61 +1,77 @@
 class Exercise {
-    constructor(id, name) {
-        this.id = id;
+    constructor(name, description) {
         this.name = name;
+        this.description = description;
     }
 }
 
-function onCreateExerciseClicked() {
-    let name = document.getElementById("exercise_name").value;
-
-    if (name === "") {
-        alert("Name cannot be empty");
-        return;
-    }
-
-    let exercisesStr = localStorage.getItem("exercises");
-    let exercises;
-
-    if (!exercisesStr) {
-        exercises = [];
-    } else {
-        exercises = JSON.parse(exercisesStr);
-    }
-
-    let newExercise = new Exercise(exercises.length + 1, name);
-
-    exercises.push(newExercise);
-
-    localStorage.setItem("exercises", JSON.stringify(exercises));
-
-    update()
-}
-
-function update() {
+async function updateExercisesList() {
     let list = document.getElementById("exercises-list");
-
-    let exercisesStr = localStorage.getItem("exercises");
-    let exercises;
-    if (!exercisesStr) {
-        exercises = [];
-    } else {
-        exercises = JSON.parse(exercisesStr);
-    }
 
     while (list.firstChild) {
         list.removeChild(list.firstChild);
     }
 
-    exercises.forEach(exercise => {
-        let section = document.createElement("section");
-        section.className = "exercise";
+    hideError();
+    showLoading();
 
-        section.appendChild(document.createTextNode(exercise.name));
+    try {
+        let postId = getRandomId();
+        let data = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+        await new Promise(resolve => setTimeout(resolve, 250)); // Immitation of long loading
 
-        list.appendChild(section)
-    });
+        let exercises = [];
+        Array.from(await data.json()).forEach(comment => {
+            let name = comment.name;
+            let description = comment.body;
+
+            exercises.push(new Exercise(name, description));
+        });
+
+        hideLoading();
+
+        exercises.forEach(exercise => {
+            let section = document.createElement("section");
+            section.className = "exercise";
+
+            exerciseName = document.createElement("h3");
+            exerciseName.appendChild(document.createTextNode(exercise.name))
+            section.appendChild(exerciseName);
+    
+            exerciseDescription = document.createElement("div");
+            exerciseDescription.appendChild(document.createTextNode("Description: " + exercise.description));
+            section.appendChild(exerciseDescription);
+    
+            list.appendChild(section)
+        });
+    } catch(error) {
+        hideLoading();
+        showError();
+    }
+}
+
+function getRandomId() {
+    const min = 1;
+    const max = 100;
+    return Math.floor(min + Math.random() * (max - min));
+}
+
+function showLoading() {
+    document.getElementById("loading-animation").className=""
+}
+
+function hideLoading() {
+    document.getElementById("loading-animation").className="hiden-element"
+}
+
+function showError() {
+    document.getElementById("error-picture").className = ""
+}
+
+function hideError() {
+    document.getElementById("error-picture").className = "hiden-element"
 }
 
 window.addEventListener('load', () => {
-    update();
+    updateExercisesList();
 });
